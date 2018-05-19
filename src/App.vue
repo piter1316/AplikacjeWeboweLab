e7f109d0ce5668daecfa3250c14701351591a80e
student4 <student4@pcoip.ki.agh.edu.pl>
2018-05-19 14:32:08 +0200

    pierwsze zadanie plus style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <h1>Twój e-mail to: {{ email}}</h1>
+    <!-- <h1>Twój e-mail to: {{ email}}</h1>
     <div v-if="email.length < 10">Ale masz krótki adres!</div>
     <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
     <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
     <input type="email" v-model="email">
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button> -->
 
     <h1>{{ welcome}}</h1>
     <div v-if="!state">
@@ -24,6 +24,7 @@
 </template>
 
 <script>
+import "milligram";
 export default {
   name: 'app',
   data () {
